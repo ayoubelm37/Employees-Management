6c14f7517dcda9ce20873f9fc300677dea3492d7
ayoub-elm211 <121443674+ayoubelm37@users.noreply.github.com>
2024-08-01 14:25:48 +0100

    first commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="bg-dark p-3 text-center text-light m-3">
       <div className="p-3">
-        <h1>Employess Management</h1>
+        <h1>Employees Management</h1>
       </div>
     </div>
   );
